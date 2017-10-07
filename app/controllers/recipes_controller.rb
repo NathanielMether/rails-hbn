18cea0d1007e73f99bd5f4ab481dc7ac0607e83c
crunchysoul <zzzcca12@gmail.com>
2017-10-07 19:54:07 +1100

    Add name to form and controller params

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -69,6 +69,6 @@ class RecipesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def recipe_params
-      params.require(:recipe).permit(:user_id, :method)
+      params.require(:recipe).permit(:name, :user_id, :method)
     end
 end
