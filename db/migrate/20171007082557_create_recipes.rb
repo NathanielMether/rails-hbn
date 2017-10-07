6abf42fbf79ac8a3137f5a894ceddf2e76ae1e2e
NathanielMether <nmet3@student.monash.edu>
2017-10-07 19:30:06 +1100

    Add everything to start

---
+++ db/migrate/20171007082557_create_recipes.rb
@@ -0,0 +1,10 @@
+class CreateRecipes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :recipes do |t|
+      t.references :user, foreign_key: true
+      t.text :method
+
+      t.timestamps
+    end
+  end
+end
